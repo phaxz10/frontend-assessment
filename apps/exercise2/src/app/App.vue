6277264b7120fe743313adef0dbfedd266d99f86
phaxz <phaxz.bautista10@gmail.com>
2024-01-16 19:34:15 +0800

    exercise 2 done

---
+++ apps/exercise2/src/app/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import data from '../../../../data.json';
+import Item from '../components/Item.vue';
+
+const activeTab = ref(0);
+
+const toggleActiveTab = (idx: number) => {
+  if (idx === activeTab.value) {
+    return (activeTab.value = -1);
+  }
+  activeTab.value = idx;
+};
+</script>
+
+<template>
+  <div class="relative flex flex-col md:flex-row md:items-center gap-4">
+    <div v-for="(item, idx) in data" :key="item.title">
+      <Item
+        :isContentVisible="activeTab === idx"
+        :onToggleVisibility="() => toggleActiveTab(idx)"
+        :title="item.title"
+        :content="item.content"
+      />
+    </div>
+  </div>
+</template>
---
+++ apps/exercise2/src/components/Item.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+defineProps<{
+  isContentVisible: boolean;
+  onToggleVisibility: () => void;
+  title: string;
+  content: string;
+}>();
+</script>
+
+<template>
+  <div class="md:max-w-max border rounded-lg w-full md:border-none">
+    <button
+      class="p-3 font-bold w-full text-left flex items-center justify-between"
+      :class="
+        isContentVisible ? 'md:border-b border-b-black md:text-blue-800' : ''
+      "
+      @click="onToggleVisibility"
+    >
+      {{ title }}
+
+      <span
+        class="md:hidden"
+        :class="isContentVisible ? '-rotate-90' : 'rotate-90'"
+        >></span
+      >
+    </button>
+    <div
+      class="overflow-hidden h-auto transition-[max-height] ease-in-out duration-300 md:absolute top-full left-0"
+      :class="
+        isContentVisible
+          ? 'block max-h-[400px] w-auto p-6 border-t border-t-black'
+          : 'max-h-0 w-0'
+      "
+      v-html="content"
+    ></div>
+  </div>
+</template>
